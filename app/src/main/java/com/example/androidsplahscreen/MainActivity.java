d78535b9800050cac4f5515cb46e40ad9bff2832
mzs21 <snigdho2111@gmail.com>
2019-12-12 22:02:55 +0600

    This task is part of GCI 2019

---
+++ app/src/main/java/com/example/androidsplahscreen/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidsplahscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
